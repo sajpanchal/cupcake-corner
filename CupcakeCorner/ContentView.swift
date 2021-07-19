afe7c88efc0aee8321a001a84ce0ec7f4142c2ec
Saj Panchal <sajpanchal@gmail.com>
2021-07-19 11:54:16 -0400

    added UI to order cupcakes and navigate to AddressView

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -8,9 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
-   
+   @ObservedObject var order = Order()
+    
     var body: some View {
-        Text("")
+        NavigationView {
+            Form {
+                Section {
+                    Picker("Select Cupcake Type", selection: $order.type) {
+                        ForEach(0..<Order.types.count, id: \.self) { index in
+                            Text(Order.types[index])
+                        }
+                    }
+                }
+                Stepper(value: $order.quantity, in: 3...20) {
+                    Text("Number of cakes: \(order.quantity)")
+                }
+                Section {
+                    Toggle(isOn: $order.specialRequestEnabled.animation(), label: {
+                        Text("Any Special Requests?")
+                    })
+                    if order.specialRequestEnabled {
+                        Toggle(isOn: $order.extraFrosting, label: {
+                            Text("Add Extra Frosting")
+                        })
+                        Toggle(isOn: $order.addSprinkles, label: {
+                            Text("Add Extra Sprinkles")
+                        })
+                    }
+                    
+                }
+                Section {
+                    NavigationLink(
+                        destination: AddressView(order: order),
+                        label: {
+                            Text("Delivery Details")
+                        })
+                }
+            }.navigationBarTitle("Cupcake Corner")
+        }
+
     }
 }
 
