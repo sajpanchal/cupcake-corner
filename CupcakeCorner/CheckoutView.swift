192aa18617237746aec5aea5c826d3143113f9cd
Saj Panchal <sajpanchal@gmail.com>
2021-07-20 11:39:15 -0400

    added custom encoding-decoding code in Order class. added http POST request-response code to CheckoutView.

--- CupcakeCorner/CheckoutView.swift
+++ CupcakeCorner/CheckoutView.swift
@@ -9,6 +9,8 @@ import SwiftUI
 
 struct CheckoutView: View {
     @ObservedObject var order: Order
+    @State var confirmationMessage = ""
+    @State var showingConfirmation = false
     var body: some View {
         GeometryReader { geometryProxy in
             ScrollView {
@@ -20,12 +22,38 @@ struct CheckoutView: View {
                     Text("Your total is $\(self.order.cost, specifier: "%.2f")")
                         .font(.title)
                     Button("Place Order") {
-                        
+                        self.placeOrder()
                     }.padding()
                 }
                
             }
+        }.alert(isPresented: $showingConfirmation, content: {
+            Alert(title: Text("Thank You"), message: Text(confirmationMessage), dismissButton: .default(Text("OK")))
+        })
+    }
+    func placeOrder() {
+        guard let encoded = try? JSONEncoder().encode(order) else {
+            print("failed to encode order")
+            return
         }
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        request.httpBody = encoded
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            guard let data = data else {
+                print("No data in response: \(error?.localizedDescription ?? "Unknown error").")
+                return
+            }
+            if let decodedOrder = try? JSONDecoder().decode(Order.self, from: data) {
+                self.confirmationMessage = "Your Order for \(decodedOrder.quantity)x\(Order.types[decodedOrder.type].lowercased()) cupcakes is on its way!"
+                self.showingConfirmation = true
+            }
+            else {
+                print("Invalid response from server.")
+            }
+        }.resume()
     }
 }
 
